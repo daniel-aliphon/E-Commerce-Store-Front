b6b710e69cc9f91a23c8ccb4feff7fa3d4ac77a2
Danx <danxgaming69@gmail.com>
2023-11-10 15:12:20 +0200

    finishing touches

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -9,25 +9,6 @@ import {
   Twitter,
 } from "lucide-react";
 
-const routes = [
-  {
-    href: "#Hero",
-    label: "Home",
-  },
-  {
-    href: "#PopularProducts",
-    label: "Popular",
-  },
-  {
-    href: "#Categories",
-    label: "Categories",
-  },
-  {
-    href: "#On-Sale",
-    label: "Deals",
-  },
-];
-
 export const Footer = () => {
   //
   const socialMediaLinks = [
@@ -54,41 +35,30 @@ export const Footer = () => {
   ];
 
   return (
-    <div className="w-full px-4 mt-48 bg-cover sm:px-6 lg:px-8 bg-secondary">
+    <div className="mt-48 w-full bg-secondary bg-cover px-4 sm:px-6 lg:px-8">
       <div
         id="Contact"
-        className="flex justify-between h-48 gap-5 p-5 mx-auto border-x max-w-7xl max-sm:h-auto max-sm:gap-9 max-sm:flex-col ">
+        className="mx-auto flex h-48 max-w-7xl justify-between gap-5 border-x p-5 max-sm:h-auto max-sm:flex-col max-sm:gap-9">
         {/*  */}
-        <div className="flex flex-col justify-between gap-1 p-3 rounded-lg max-sm:items-center sm:pr-2 min-w-max">
+        <div className="flex min-w-max flex-col justify-between gap-1 rounded-lg p-3 max-sm:items-center sm:pr-2">
           <h1 className="text-2xl font-bold">#Store Name</h1>
 
-          <p>
-            |
-            {routes.map((route) => (
-              <a
-                key={route.label}
-                href={route.href}>
-                {" "}
-                {route.label} |
-              </a>
-            ))}
-          </p>
           <div className="flex gap-3">
             {socialMediaLinks.map((link) => (
               <Button
                 key={link.name}
-                className="">
+                className="rounded-full">
                 {link.icon}
               </Button>
             ))}
           </div>
           <p className="flex items-center gap-2 text-lg">
-            <Copyright /> copyright
+            <Copyright /> 2023 Daniel Aliphon
           </p>
         </div>
 
-        <div className="flex flex-col items-center justify-around text-center rounded-lg max-sm:hidden">
-          <h1 className="text-2xl font-semibold ">About Us</h1>
+        <div className="flex flex-col items-center justify-around rounded-lg text-center max-sm:hidden">
+          <h1 className="text-2xl font-semibold">About Us</h1>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
             odio quo, quia labore non fuga! At ea esse non nam atque dicta
@@ -96,13 +66,10 @@ export const Footer = () => {
           </p>
         </div>
 
-        <div className="flex flex-col items-end pr-4 rounded-lg max-sm:items-center max-sm:text-center max-sm:p-4 justify-evenly text-end min-w-48">
+        <div className="min-w-48 flex flex-col items-end justify-evenly rounded-lg pr-4 text-end max-sm:items-center max-sm:p-4 max-sm:text-center">
           <ContactButton />
 
-          <p>
-            Lorem ipsum dolor sit amet adipisicing elit. Quisquam asperiores
-            blanditiis sed.
-          </p>
+          <p>Lorem ipsum dolor sit amet adipisicing elit.</p>
         </div>
       </div>
     </div>
